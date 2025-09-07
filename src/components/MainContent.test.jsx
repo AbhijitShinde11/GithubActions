3f444a9826dce0c0d30ea72fe23a33d7edf68699
abhijit-space101 <abhijit.shinde@space.ge>
2025-09-07 13:24:48 +0530

    added lint and updated workflows

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -1,5 +1,6 @@
+import React from 'react';
 import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
+import { render, screen, describe, it, expect } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import MainContent from './MainContent';
