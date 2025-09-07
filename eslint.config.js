334d6d843c8f9c1a11a9d4460c6384f65154d312
abhijit-space101 <abhijit.shinde@space.ge>
2025-09-07 13:38:51 +0530

    updated eslint config and removed unused imports in test

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,31 @@ import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: globals.browser,
+    }
+  },
+
   pluginReact.configs.flat.recommended,
+
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off"
+    },
+  },
+
+  {
+    files: ["**/*.test.{js,jsx}", "**/__tests__/**/*.{js,jsx}"],
+    languageOptions: {
+      globals: {
+        ...globals.jest,
+      },
+    },
+  },
 ]);
